ffa04a421b40084d893b05c1d4c4c45ae404e448
kelechukwu213 <kelechukwu213@gmail.com>
2023-03-28 10:55:53 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -15,6 +16,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s)
+
 #endif
+
 
